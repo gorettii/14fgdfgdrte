90329b54ea83761ef555b79ad8e88afcc6515192
gorettii <101406498+gorettii@users.noreply.github.com>
2022-03-18 13:57:38 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+basic.forever(function () {
+	
+})
